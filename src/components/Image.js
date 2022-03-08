6e97814bc8f5652de97a10008afe47a78e0eee2f
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-03-08 21:40:42 +0100

    adds eventhandler for tracking clicked position in image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { imageSrc } from '../helpers/imageSrc';
 import TargetBox from './TargetBox';
 
 const Image = (props) => {
     // TODO: gets chosen image (= index in imageSrc array) as property to display
+
     const handleClick = (e) => {
         e.preventDefault();
         e.currentTarget.classList.toggle('isSelected');
     }
+
+    const getMousePosition = (canvas, e) => {
+        let rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+        console.log("Coordinate x: " + x, 
+                    "Coordinate y: " + y);
+    }
+
+    useEffect(() => {
+        const canvasElem = document.querySelector('.c-image');
+        canvasElem.addEventListener('mousedown', (e) => {
+            getMousePosition(canvasElem, e);
+        });
+
+        return () => {
+            canvasElem.removeEventListener('mousedown', (e) => {
+                getMousePosition(canvasElem, e);
+            });
+        }
+    });
+
     return (
         <div className='o-wrap c-image'>
             <img src={imageSrc[0].url} alt={imageSrc[0].alt} useMap='#view' />
